e370df8140c898ecb5599fd0f8495cb362d8a19e
jitu <jitendrakumarpalei6@gmail.com>
2024-08-19 13:13:09 +0530

    my commit

--- main.tf
+++ main.tf
@@ -4,10 +4,33 @@ provider "aws" {
 
 resource "aws_vpc" "my-vpc" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name: "development"
+    vpc_env: "dev"
+  }
 }
 
 resource "aws_subnet" "my-subnet" {
   vpc_id = aws_vpc.my-vpc.id
   cidr_block = "10.0.10.0/24"
   availability_zone = "us-east-2a"
+   tags = {
+    Name: "subnet-1-development"
+  }
+}
+
+data "aws_vpc" "existing_vpc" {
+  default = true
+   
+}
+
+
+resource "aws_subnet" "my-vpc1" {
+  vpc_id = data.aws_vpc.existing_vpc.id
+  cidr_block = "172.31.48.0/20"
+  availability_zone = "us-east-2a"
+   tags = {
+    Name: "subnet-1-default"
+  }
+  
 }
